c8733aaa135409d83e87697bfcfd228f499796db
Alexander Flatter <flatter@fastmail.fm>
2013-06-07 02:24:41 +0200

    Add TestHelper

---
+++ lib/raft/test_helper.rb
@@ -0,0 +1,15 @@
+require 'raft'
+
+module Raft::TestHelper
+  extend self
+
+  # Takes a list of node ids and yields a list of peers for each id.
+  def cluster(ids, &block)
+    cluster_size = ids.size
+
+    cluster_size.times.map do |i|
+      combination = ids.rotate(i)
+      block.call(combination.first, combination[1..-1])
+    end
+  end
+end
